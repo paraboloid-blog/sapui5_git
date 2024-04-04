6f98e84f90cbf32dbd66c6bbaf56623d216cefdf
box-for-spam <box-for-spam@protonmail.ch>
2024-04-04 21:58:19 +0200

    Detail view and controller added

---
+++ ux402_repository/webapp/controller/Detail.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller) {
+        "use strict";
+
+        return Controller.extend("student00.com.sap.training.ux402.repository.ux402repository.controller.Detail", {
+            onInit: function () {
+
+            }
+        });
+    });
